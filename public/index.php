f7b5ae625fc6a9e46365e14f8f2f31af53fb7feb
Aleksandr Mironov <mironovs.2004@gmail.com>
2024-11-17 15:21:59 +0200

    20.2 complete

--- public/index.php
+++ public/index.php
@@ -1,3 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Todoist</title>
+</head>
+<body>
+    <section class = "content">
+        <header>
+        <span class = "icon">📝</span>
+        <h1>Todoist</h1>
+        </header>
+        <main>
+            <article class = "todo">
+                <label>
+                    <input type="checkbox" checked>
+                    My todo 1
+                </label>
+            </article>
+            <article class = "todo">
+                <label>
+                    <input type="checkbox" checked>
+                    My todo 2
+                </label>
+            </article>
 
-echo "hi";
+            <form action="/" method="post" class = "add-todo">
+                <input type="text" placeholder="What to do?">
+                <button type="submit">Add</button>
+            </form>
+        </main>
+        <footer>
+            &copy; 2024 myproject
+        </footer>
+    </section>
+</body>
+</html>
